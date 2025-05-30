6beadd25cf5c7644cd753e24b290e15fa9daeeeb
Venish Patidar <venishpatidar@gmail.com>
2025-05-30 15:26:06 -0700

    Merged Tokenizer path and Model path into one, which expect both weights to be in one directory

--- model/config.py
+++ model/config.py
@@ -1,7 +1,6 @@
 class Config:
     def __init__(self):
         # tokenizer and weight path (either used the pretrained or new one) 
-        self.TOKENIZER_PATH = "./T5-fine-tuned-NLP-JSON"
         self.MOEDL_PATH = "./T5-fine-tuned-NLP-JSON"
 
 config = Config()
